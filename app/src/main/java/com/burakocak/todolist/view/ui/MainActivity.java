5b9e911fa7ebf8a55e1a0a28c5fee5be579762e7
Burak <burakocak@teknasyon.com>
2019-08-12 02:12:40 +0300

    test added todo

--- app/src/main/java/com/burakocak/todolist/view/ui/MainActivity.java
+++ app/src/main/java/com/burakocak/todolist/view/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.burakocak.todolist.view.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 import androidx.databinding.DataBindingUtil;
@@ -18,6 +20,7 @@ import com.burakocak.todolist.view.base.BaseActivity;
 import com.burakocak.todolist.viewmodel.MainViewModel;
 
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends BaseActivity {
 
@@ -25,13 +28,14 @@ public class MainActivity extends BaseActivity {
     private MainViewModel mainViewModel;
     private ActivityMainBinding binding;
     private TodoListAdapter todoListAdapter;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String username = getIntent().getStringExtra("username");
         binding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
         mainViewModel = ViewModelProviders.of(MainActivity.this).get(MainViewModel.class);
+        username  = getIntent().getStringExtra("username");
         mainViewModel.setAllTodo(username);
         mainViewModel.getAllTodo().observe(this, new Observer<List<TodoList>>() {
             @Override
@@ -40,7 +44,17 @@ public class MainActivity extends BaseActivity {
             }
         });
         init();
+        binding.btnAddTodo.setOnClickListener(clickListener);
 
+    }
+
+    public void addTodotest(){
+        TodoList todoList =  new TodoList();
+        Random r = new Random();
+        int a = r.nextInt();
+        todoList.setTitle("TODO LIST"+a);
+        todoList.setUser(username);
+        mainViewModel.addTodo(todoList);
     }
 
     private void init() {
@@ -50,7 +64,19 @@ public class MainActivity extends BaseActivity {
         binding.rvTodoList.setAdapter(todoListAdapter);
     }
 
-    @Override
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_add_todo:
+                    addTodotest();
+                    mainViewModel.getAllTodo();
+            }
+        }
+    };
+
+        @Override
     public void onBackPressed() {
         showExitApplicationDialog("Closing Activity","Are you sure you want to close this activity?");
     }
