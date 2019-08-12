7b531fa6f0552ae19eaf42e0393abb2b63cfc09d
Burak <burakocak@teknasyon.com>
2019-08-12 18:32:46 +0300

    added activity data transfer

--- app/src/main/java/com/burakocak/todolist/view/ui/TodoListItemActivity.java
+++ app/src/main/java/com/burakocak/todolist/view/ui/TodoListItemActivity.java
@@ -1,9 +1,11 @@
 package com.burakocak.todolist.view.ui;
 
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +16,8 @@ import com.burakocak.todolist.model.TodoItem;
 import com.burakocak.todolist.view.adapter.TodoListItemAdapter;
 import com.burakocak.todolist.view.base.BaseActivity;
 import com.burakocak.todolist.viewmodel.TodoListItemViewModel;
+
+import static com.burakocak.todolist.utils.Constants.ADD_TODO_REQUEST;
 
 
 public class TodoListItemActivity extends BaseActivity implements TodoListItemAdapter.OnDeleteButtonItemClickListener {
@@ -53,12 +57,13 @@ public class TodoListItemActivity extends BaseActivity implements TodoListItemAd
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.btn_add_todo_item:
-                    TodoItem todoItem = new TodoItem();
+                    /*TodoItem todoItem = new TodoItem();
                     todoItem.setTitle("Test Title");
                     todoItem.setDesc("Test title Test title Test title v v Test title Test title Test title");
                     todoItem.setExpiredDate("12/12/2019");
                     todoItem.setTodoId(todoId);
-                    viewModel.addTodoItem(todoItem);
+                    viewModel.addTodoItem(todoItem);*/
+                    startActivityForResult(new Intent(TodoListItemActivity.this,AddTodoItemActivity.class),ADD_TODO_REQUEST);
             }
         }
     };
@@ -72,6 +77,25 @@ public class TodoListItemActivity extends BaseActivity implements TodoListItemAd
     @Override
     public void onCustomEvent(EventbusObject eventbusObject) {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == ADD_TODO_REQUEST && resultCode == RESULT_OK) {
+            String itemTitle = data.getStringExtra(AddTodoItemActivity.TODO_ITEM_TITLE);
+            String itemDesc = data.getStringExtra(AddTodoItemActivity.TODO_ITEM_DESC);
+            String itemDate = data.getStringExtra(AddTodoItemActivity.TODO_ITEM_DATE);
+            TodoItem todoItem = new TodoItem();
+            todoItem.setTitle(itemTitle);
+            todoItem.setDesc(itemDesc);
+            todoItem.setExpiredDate(itemDate);
+            todoItem.setTodoId(todoId);
+            todoItem.setCompleted(false);
+            viewModel.addTodoItem(todoItem);
+            showSuccessSneaker("Save!","New todo is add to do list");
+        } else {
+            showErrorSneaker("Not Save!!","New todo is not saved!!");
+        }
     }
 
     @Override
